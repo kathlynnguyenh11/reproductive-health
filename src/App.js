c8bc4d3a06946fb73f1a4774f4b7503d46f89615
Kathlyn Hang Nguyen <kathlyn.hang.nguyen11@gmail.com>
2020-09-19 15:46:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App (){
       <BrowserRouter>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-2">
-           
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
